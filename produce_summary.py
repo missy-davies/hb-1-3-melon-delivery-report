0b2d824b7630e36d42b89ac8d177d95f4d257776
Missy Davies <ms.melissadavies@gmail.com>
2021-02-25 05:13:14 +0000

    Refactored code to be more succinct with a function

--- produce_summary.py
+++ produce_summary.py
@@ -1,43 +1,23 @@
-print("Day 1")
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+def print_daily_report(day, the_file):
+    """Given a day number and a path to a file generate a report
+    
+    Generate a report for the number of melons and the total cost delivered for a particular day"""
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+    log = open(the_file)
+    print(f"Day {day}")
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+    for line in log:
+        line = line.rstrip()
+        words = line.split('|')
 
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
 
-print("Day 2")
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+        print(f"Delivered {count} {melon}s for total of ${amount}")
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+    log.close()
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
-
-
-print("Day 3")
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+print_daily_report(1, "um-deliveries-20140519.txt")
+print_daily_report(2, "um-deliveries-20140520.txt")
+print_daily_report(3, "um-deliveries-20140521.txt")
